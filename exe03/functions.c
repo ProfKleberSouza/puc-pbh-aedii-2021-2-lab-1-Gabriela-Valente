7b893894fed91d67363d8aed39b74aaac8596248
gabrielalv <88516987+Gabriela-Valente@users.noreply.github.com>
2021-08-13 23:51:09 +0000

    exercicio 3

--- exe03/functions.c
+++ exe03/functions.c
@@ -1,7 +1,19 @@
 
 #include "functions.h"
 
-int somar(int n1, int n2){
-
-    return n1+n2;
+void menor_maior(int v[], int n, int *menor, int *maior){
+  for(int i = 0; i < n; i++){
+    if(i == 0){
+      *menor = v[0];
+      *maior = v[0];
+    }
+    else{
+      if(v[i] > *maior){
+        *maior = v[i];
+      }
+      if(v[i] < *menor){
+        *menor = v[i];
+      }
+    }
+  }
 }
--- exe03/main.c
+++ exe03/main.c
@@ -2,17 +2,13 @@
 #include "functions.h"
 
 int main() {
-
-    int a, b, s;
-  
-  // ENTRADA DE DADOS
-  scanf("%d %d", &a, &b);
-
-  // PROCESSAMENTO - EXECUTANDO A FUNCAO
-  s = somar(a, b);
-
-  // SAIDA - IMPRIMINDO O RESULTADO
-  printf("SOMA = %d\n", s);
-
-  return(0);
+  int n, menor, maior;
+  scanf("%d", &n);
+  int vet[n];
+  for(int i = 0; i < n; i++){
+    scanf("%d", &vet[i]);
+  }
+  menor_maior(vet, n, &menor, &maior);
+  printf("MENOR = %d\n", menor);
+  printf("MAIOR = %d\n", maior);
 }
