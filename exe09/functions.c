2c7d070792dd715701c1c7d2658b4bea9d83086f
gabrielalv <88516987+Gabriela-Valente@users.noreply.github.com>
2021-08-20 23:16:34 +0000

    exercicio 9

--- exe09/functions.c
+++ exe09/functions.c
@@ -1,61 +1,27 @@
 #include <stdio.h>
-#include <stdlib.h>
 #include <string.h>
-#include <stdbool.h>
+void placar_possiveisRec(char placar[], int m, int n, int pos){
+  
+  if(m == 0 && n == 0){
+    placar[pos] = '\0';
+    printf("%s\n", placar);
+  }
+  else{
+    if(m > 0){
+      placar[pos] = 'A';
+      placar_possiveisRec(placar, m - 1, n, pos+1);
 
-void troca(char* a, char* b){
-  char aux = *a;
-  *a = *b;
-  *b = aux;
-}
-  
-// Essa função acha o ídice do menor caracter que é maior que o primeiro da string[l...h]
-int acha_indice(char str[], char primeiro, int tamI, int tamF){
-  int Indice = tamI;
-  for (int i = tamI + 1; i <= tamF; i++){
-    if (str[i] > primeiro && str[i] < str[Indice]){
-      Indice = i;
     }
-  }
-  return Indice;
-}
-  
-void imprime(char str[]){
-  int tam = strlen(str);
-  bool isFinished = false;
-  while (!isFinished) {
-    printf("%s\n", str);
-    int i;
-    //verifica o menor caracter que tem a sua direita um maior
-    for (i = tam - 2; i >= 0; --i){
-      if (str[i] < str[i + 1])
-        break;
+     
+     if(n > 0){
+      placar[pos] = 'B';
+      placar_possiveisRec(placar, m, n - 1, pos+1);
     }
-    //caso contrario esta em ordem decrescente
-    if (i == -1)
-      isFinished = true;
-    else {
-      // acha o ídice do menor caracter que é maior que o primeiro da string
-      int Indice = acha_indice(str, str[i], i + 1, tam - 1);
-      // troca primeiro e o segundo caracter
-      troca(&str[i], &str[Indice]);
-    }
+     
   }
 }
-
-  void placar_possiveis(int m, int n){
-  int x = 0;
-  char vet[x];
-  x = m + n;
-  for(int i = 0; i < x; i++){
-    if(i < m){
-      vet[i] = 'A';
-    }
-    else{
-      vet[i] = 'B';
-    }
-  }
+void placar_possiveis(int m, int n){
+  char placar[100];
+  placar_possiveisRec(placar, m, n, 0);
   
-  imprime(vet);
-  printf("\n");
 }
--- exe09/functions.h
+++ exe09/functions.h
@@ -1,4 +1,2 @@
-void troca(char* a, char* b);
-int acha_indice(char str[], char primeiro, int tamI, int tamF);
-void imprime(char str[]);
+void placar_possiveisRec(char placar[], int m, int n, int pos);
 void placar_possiveis(int m, int n);
